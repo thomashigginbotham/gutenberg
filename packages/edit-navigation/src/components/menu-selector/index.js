3236954e25ce769d924661b2d180948e49007d27
grzegorz_marzencki <grzegorz.marzencki@a8c.com>
2021-02-24 12:10:16 +0100

    use Card component in MenuSelector instead of custom styles

--- packages/edit-navigation/src/components/menu-selector/index.js
+++ packages/edit-navigation/src/components/menu-selector/index.js
@@ -3,27 +3,31 @@
  */
 import {
 	MenuGroup,
-	MenuItemsChoice
+	MenuItemsChoice,
+	Card,
+	CardBody,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 export default function MenuSelector( { onSelectMenu, menus } ) {
 	return (
-		<div className="edit-navigation-menu-selector">
-			<h3 className="edit-navigation-menu-selector__header">
-				{ __( 'Choose a menu to edit: ' ) }
-			</h3>
-			<div className="edit-navigation-menu-selector__body">
-				<MenuGroup>
-					<MenuItemsChoice
-						onSelect={ onSelectMenu }
-						choices={ menus.map( ( menu ) => ( {
-							value: menu.id,
-							label: menu.name,
-						} ) ) }
-					/>
-				</MenuGroup>
-			</div>
-		</div>
+		<Card className="edit-navigation-menu-selector">
+			<CardBody>
+				<h3 className="edit-navigation-menu-selector__header">
+					{ __( 'Choose a menu to edit: ' ) }
+				</h3>
+				<div className="edit-navigation-menu-selector__body">
+					<MenuGroup>
+						<MenuItemsChoice
+							onSelect={ onSelectMenu }
+							choices={ menus.map( ( menu ) => ( {
+								value: menu.id,
+								label: menu.name,
+							} ) ) }
+						/>
+					</MenuGroup>
+				</div>
+			</CardBody>
+		</Card>
 	);
 }
